13335d90013aee03aae289bd6e0c28ba8d351bee
brendapalg <bpalgui@hotmail.com>
2022-04-24 21:53:41 -0400

    added examples in trends view

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
 from application import app
-from flask import render_template
+from flask import render_template, json, request
 import requests
+
 
 # decorator to access the app
 @app.route("/")
@@ -11,15 +12,28 @@ def home():
 
 @app.route("/trends", methods = ['GET', 'POST'])
 def trends():
-    url = "https://swd-model.herokuapp.com/trends"
-    response = requests.get(url)
-    graphJSON = response.content.decode("UTF-8")
-    return render_template("trends.html", graphJSON=graphJSON)
+    date = request.form.get("date")
+    url = 'http://127.0.0.1:5000/trends'#"https://swd-model.herokuapp.com/trends"
+    
+
+    if date is None:
+        response = requests.get(url)
+        template="trends.html"
+    else:
+        input_data = json.dumps({"date": date})
+        response = requests.post(url, input_data)
+        template="trendsAndExamples.html"
+    
+    data = json.loads(response.content.decode("UTF-8"))
+    graphJSON = data['graphJSON']
+    pos = data['pos']
+    neg = data['neg']
+    return render_template(template, graphJSON=graphJSON, pos = pos, neg=neg)
 
 
 @app.route("/topics", methods = ['GET', 'POST'])
 def topics():
-    url = "https://swd-model.herokuapp.com/topics"
+    url = "http://127.0.0.1:5000//topics"
     response = requests.get(url)
     topics_html = response.content.decode("UTF-8")
     return topics_html
